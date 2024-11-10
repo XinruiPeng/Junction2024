7e32cda0085da3e51ca53e97dbc13f17cbee6b5b
Jeremy Jin <jeremy@Jeremys-MBP.lan>
2024-11-10 08:38:35 +0200

    new login page

--- junction2024-sitra-challenge/src/App.js
+++ junction2024-sitra-challenge/src/App.js
@@ -3,18 +3,23 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RecaptchaForm from "./components/RecaptchaForm";
 import LoginForm from "./components/LoginForm";
 import DAO from "./components/DAO";
+import Navbar from "./components/NavBar"; // Import Navbar component
 import "./App.css"; // Import CSS for styling
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<LoginForm />} />  {/* Login Route */}
-        <Route path="/forum" element={<DAO />} />  {/* DAO Route */}
-        <Route path="/" element={<RecaptchaForm />} />  {/* Default Route */}
-      </Routes>
+      <Navbar /> {/* Navbar is now fixed at the top */}
+      <div className="main-content"> {/* Wrapper for the main content */}
+        <Routes>
+          <Route path="/login" element={<LoginForm />} />  {/* Login Route */}
+          <Route path="/forum" element={<DAO />} />  {/* DAO Route */}
+          <Route path="/" element={<RecaptchaForm />} />  {/* Default Route */}
+        </Routes>
+      </div>
     </Router>
   );
 };
 
 export default App;
+
